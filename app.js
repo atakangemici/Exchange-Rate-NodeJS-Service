d610b7d2a10e5cd0ca8e17313dd5ad03e7201d2a
Atakan Gemici <atakan.gemici@ofisim.com>
2020-08-06 16:52:35 +0300

    exchange rate services

---
+++ app.js
@@ -0,0 +1,26 @@
+const xmlToJson = require('xml-to-json-stream');
+const parser = xmlToJson({attributeMode:false});
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/exchange_rate', (req, res) => {
+ var request = require('request');
+request('http://www.tcmb.gov.tr/kurlar/today.xml', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    
+    parser.xmlToJson(body, (err,json)=>{
+        var exchange = {};
+        exchange["name"] = json["Tarih_Date"]["Currency"][0]["Isim"];
+        exchange["rate"] = json["Tarih_Date"]["Currency"][0]["BanknoteSelling"];
+
+        res.send(exchange);     
+    });
+
+  }
+})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
