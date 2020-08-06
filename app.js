f234196855e635faebfb7cc4e06450bebd215f8d
Atakan Gemici <atakan.gemici@ofisim.com>
2020-08-06 20:59:45 +0300

    bugfix.

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ request('http://www.tcmb.gov.tr/kurlar/today.xml', function (error, response, bo
 })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
